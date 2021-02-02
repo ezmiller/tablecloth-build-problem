1c44affed3f4a0e9e25b5a5829efa319156665a5
Ethan Miller <ethanzanemiller@gmail.com>
2021-02-01 20:58:20 -0800

    first commit

---
+++ test/myapptest/core_test.clj
@@ -0,0 +1,8 @@
+(ns myapptest.core-test
+  (:require [clojure.test :refer :all]
+            [tablecloth.api :as tablecloth]
+            [myapptest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
